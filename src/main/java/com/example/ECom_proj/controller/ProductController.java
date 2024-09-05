0633760e8d7deb9f623e166593ff4d6f2d5ec22d
Avi Tomar <avitomar12@hotmail.com>
2024-09-05 17:41:18 +0200

    formatted the response from list to responseentity

--- src/main/java/com/example/ECom_proj/controller/ProductController.java
+++ src/main/java/com/example/ECom_proj/controller/ProductController.java
@@ -2,8 +2,12 @@ package com.example.ECom_proj.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseCookie.ResponseCookieBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,7 @@ import com.example.ECom_proj.model.Product;
 import com.example.ECom_proj.service.ProductService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
@@ -26,7 +31,11 @@ public class ProductController {
         return "Hello from ProductController";
     }
     @GetMapping("/products")
-    public List<Product> getProducts(){
-        return service.getAllProducts();
+    public ResponseEntity <List<Product>> getProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        return new ResponseEntity<>(service.getProductById(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/ECom_proj/model/Product.java
+++ src/main/java/com/example/ECom_proj/model/Product.java
@@ -3,6 +3,8 @@ package com.example.ECom_proj.model;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -26,6 +28,7 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date release_date;
     private boolean available;
     private int quantity;
